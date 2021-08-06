1727c1f74bb2d8355c4419c6446c567becfd10b1
Ivan Urra <ivan.urra.wd@gmail.com>
2021-08-06 18:01:51 +0200

    Styled components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import React from "react";
 import Header from "./components/Header";
+import styled from "@emotion/styled";
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color:#FFF;
+  padding: 3rem;
+`;
 
 function App() {
   return (
-    <Header titulo='CAR INSURANCE APP'/>
+    <Contenedor>
+      <Header
+      titulo="CAR INSURANCE APP"
+      />
+      <ContenedorFormulario>
+
+      </ContenedorFormulario>
+    </Contenedor>
   );
 }
 
