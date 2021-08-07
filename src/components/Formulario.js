199e68e8c321dbe062fc0caaa1653d7cd7c5a03a
Ivan Urra <ivan.urra.wd@gmail.com>
2021-08-07 20:13:44 +0200

    styled comp

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,20 +1,43 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const Campo = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+  align-items: center;
+`;
+
+const Label = styled.label`
+  flex: 0 0 100px; 
+`;
+
+const Select = styled.select`
+  display: block;
+  width: 100%;
+  padding: 1rem;
+  border: 1px solid #e1e1e1;
+  -webkit-appearance: none;
+`;
+
+const InputRadio = styled.input`
+  margin: 0 0.5rem;
+`;
 
 const Formulario = () => {
   return (
     <form>
-      <div>
-        <label>Marca:</label>
-        <select>
+      <Campo>
+        <Label>Marca:</Label>
+        <Select>
           <option value="">--Seleccione--</option>
           <option value="americano">Americano</option>
           <option value="europeo">Europeo</option>
           <option value="asiatico">Asiático</option>
-        </select>
-      </div>
-      <div>
-        <label>Año:</label>
-        <select>
+        </Select>
+      </Campo>
+      <Campo>
+        <Label>Año:</Label>
+        <Select>
           <option value="">-- Seleccione --</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
@@ -26,13 +49,13 @@ const Formulario = () => {
           <option value="2014">2014</option>
           <option value="2013">2013</option>
           <option value="2012">2012</option>
-        </select>
-      </div>
-      <div>
-        <label>Plan:</label>
-        <input type="radio" name="plan" value="basico" />
-        <input type="radio" name="plan" value="completo" />
-      </div>
+        </Select>
+      </Campo>
+      <Campo>
+        <Label>Plan:</Label>
+        <InputRadio type="radio" name="plan" value="basico" /> Básico
+        <InputRadio type="radio" name="plan" value="completo" /> Completo
+      </Campo>
       <button type="button">Cotizar</button>
     </form>
   );
