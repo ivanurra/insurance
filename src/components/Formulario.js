4a90a1a5575e7fca4a23341c344277782aa8fc07
Ivan Urra <ivan.urra.wd@gmail.com>
2021-08-08 17:19:56 +0200

    calc cost year

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styled from "@emotion/styled";
+import { obtenerDiferenciaYear } from '../helper';
 
 const Campo = styled.div`
   display: flex;
@@ -83,9 +84,17 @@ const Formulario = () => {
 
     guardarError(false);
 
+    // Una base de 2000
+    let resultado = 2000;
+
     // obtener la diferencia de años
 
+    const diferencia = obtenerDiferenciaYear(year);
+    console.log(diferencia);
+
     // por cada año hay que restar el 3%
+
+    resultado -= (( diferencia * 3) * resultado) / 100;
 
     // Americano 15%
     // Asiático 5%
